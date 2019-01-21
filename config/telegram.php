a5a3668eabca8fb401d575d521fdea8e307c4317
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-21 09:02:21 +0200

    fix

--- config/telegram.php
+++ config/telegram.php
@@ -39,7 +39,7 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                Telegram\Bot\Commands\HelpCommand::class,
+//                Telegram\Bot\Commands\HelpCommand::class,
 //                App\Telegram\TestCommand::class,
                 App\Telegram\FirstCommand::class,
             ],
